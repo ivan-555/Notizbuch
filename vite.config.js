10c1707c0e9fcf279c3cf16bdeff854369fa54ea
Ivan Facchinetti <ivan.facchinetti55@gmail.com>
2025-08-05 15:47:31 +0200

    PWA config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.png',
+        'pwa-192x192.png',
+        'pwa-512x512.png'
+      ],
+      manifest: {
+        name: 'Notizbuch',
+        short_name: 'Notes',
+        description: 'React Notizbuch',
+        theme_color: '#4b4948',
+        background_color: '#0000000f',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   base: "/Notizbuch/",
 })
